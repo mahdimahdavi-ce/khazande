df16ad8a3db35bb218a7cbbf4f1f1ceb0a9b71a8
mahdi <mahdi.mahdavi.maz@gmail.com>
2024-06-19 11:10:50 +0330

    feat(main): FetchVulnerabilities() is implemented which is responsible serving vulnerabilites to other services

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,71 @@
 package main
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
+
+	crawlerModule "khazande/internal/crawler"
+	pb "khazande/internal/grpc"
+)
+
+type server struct {
+	pb.UnimplementedScrapperServiceServer
+	Logger      *zap.Logger
+	RedisClient *redis.Client
+}
+
+func handlePanic() {
+	a := recover()
+
+	if a != nil {
+		fmt.Printf("Recover from panic: %v", a)
+	}
+}
+
+func (s *server) FetchVulnerabilities(ctx context.Context, req *pb.VulnerabilityRequest) (*pb.VulnerabilityResponse, error) {
+	defer handlePanic()
+
+	query := req.GetName()
+	s.Logger.Info(fmt.Sprintf("Start searching for %s vulnerabilities", query))
+
+	crawler := crawlerModule.Crawler{Logger: s.Logger, RedisClient: s.RedisClient}
+
+	links := crawler.ExtractVulnerabilitiesLinks(query)
+
+	if len(links) == 0 {
+		return nil, fmt.Errorf("there is no matching Vulnerabilities")
+	}
+
+	vulnerabilities := crawler.ExtractVulnerabilitiesDetails(query, links)
+
+	if len(vulnerabilities) != 0 {
+		s.Logger.Info(fmt.Sprintf("Web Scrapper has extracted %d vulnerabilities successfully!", len(vulnerabilities)))
+	} else {
+		s.Logger.Info(fmt.Sprintf("Failed to extract vulnerabilities for %s", query))
+	}
+
+	result := []*pb.Vulnerability{}
+	for _, vulnerability := range vulnerabilities {
+		result = append(result, &pb.Vulnerability{
+			Name:               vulnerability.Name,
+			CVEID:              vulnerability.CVEID,
+			PublishedDate:      vulnerability.PublishedDate,
+			LastModified:       vulnerability.LastModified,
+			Description:        vulnerability.Description,
+			VulnerableVersions: vulnerability.VulnerableVersions,
+			NVDScore:           vulnerability.NVDScore,
+			CNAScore:           vulnerability.CNAScore,
+		})
+	}
+
+	return &pb.VulnerabilityResponse{
+		Vulnerabilities: result,
+	}, nil
+}
+
 func main() {
 
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/gocolly/colly v1.2.0
 	github.com/redis/go-redis/v9 v9.5.3
 	go.uber.org/zap v1.27.0
+	google.golang.org/grpc v1.64.0
+	google.golang.org/protobuf v1.33.0
 )
 
 require (
@@ -18,13 +20,14 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
 )
