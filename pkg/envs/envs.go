3a3310d1f18517c7c59a0a69551e476ef9cf5866
Mahdi <mahdi.mahdavi.maz@gmail.com>
2024-08-03 21:21:10 +0330

    feat(envs): new envs are added

--- pkg/envs/envs.go
+++ pkg/envs/envs.go
@@ -3,11 +3,13 @@ package envs
 import "os"
 
 type Envs struct {
-	GRPC_SERVER_ADDRESS string
-	GRPC_SERVER_PORT    string
-	LOG_LEVEL           string
-	REDIS_ADDRESS       string
-	REDIS_PORT          string
+	GRPC_SERVER_ADDRESS          string
+	GRPC_SERVER_PORT             string
+	LOG_LEVEL                    string
+	REDIS_ADDRESS                string
+	REDIS_PORT                   string
+	GITHUB_ADVISORT_DATABASE_URL string
+	GITHUB_TOKEN                 string
 }
 
 func ReadEnvs() *Envs {
@@ -17,6 +19,8 @@ func ReadEnvs() *Envs {
 	envs.LOG_LEVEL = os.Getenv("LOG_LEVEL")
 	envs.REDIS_ADDRESS = os.Getenv("REDIS_ADDRESS")
 	envs.REDIS_PORT = os.Getenv("REDIS_PORT")
+	envs.GITHUB_ADVISORT_DATABASE_URL = os.Getenv("GITHUB_ADVISORT_DATABASE_URL")
+	envs.GITHUB_TOKEN = os.Getenv("GITHUB_TOKEN")
 
 	return &envs
 }
