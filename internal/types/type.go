400c7b08d840d4e7afaf00c14cc3fe8c2cc21732
Mahdi <mahdi.mahdavi.maz@gmail.com>
2024-08-03 20:22:14 +0330

    feat(internal): types package is added which contains graphQL response types and etc ...

---
+++ internal/types/type.go
@@ -0,0 +1,47 @@
+package types
+
+import "time"
+
+type Vulnerability struct {
+	Name               string   `json:"name"`
+	CVEID              string   `json:"CVEID"`
+	PublishedDate      string   `json:"publishDate"`
+	LastModified       string   `json:"lastModified"`
+	Description        string   `json:"description"`
+	VulnerableVersions []string `json:"vulnerableVersions"`
+	NVDScore           string   `json:"NVDScore"`
+	CNAScore           string   `json:"CNAScore"`
+	AffectedVersions   string   `json:"affectedVersions"`
+	PatchedVersions    string   `json:"patchedVersions"`
+}
+
+type GitHubVulnerabilityQueryResponse struct {
+	Data struct {
+		SecurityVulnerabilities struct {
+			Nodes []VulnerabilityNode `json:"nodes"`
+		} `json:"securityVulnerabilities"`
+	} `json:"data"`
+}
+
+type VulnerabilityNode struct {
+	Package struct {
+		Name string `json:"name"`
+	} `json:"package"`
+	Advisory struct {
+		Summary     string       `json:"summary"`
+		Description string       `json:"description"`
+		Severity    string       `json:"severity"`
+		Identifiers []Identifier `json:"identifiers"`
+		PublishedAt time.Time    `json:"publishedAt"`
+	} `json:"advisory"`
+	VulnerableVersionRange string `json:"vulnerableVersionRange"`
+	FirstPatchedVersion    struct {
+		Identifier string `json:"identifier"`
+	} `json:"firstPatchedVersion"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type Identifier struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
