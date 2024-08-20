f769a7395875c3a6013eef0940d58755a0cc4ba3
Mahdi <mahdi.mahdavi.maz@gmail.com>
2024-08-20 11:45:25 +0330

    feat(*): cors middleware is added

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"os/signal"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"google.golang.org/grpc"
 
@@ -25,6 +26,7 @@ import (
 func main() {
 	app := fiber.New()
 	app.Use(logger.New())
+	app.Use(cors.New())
 
 	envs := envsModule.ReadEnvs()
 	logger := loggerModule.InitialLogger(envs.LOG_LEVEL)
