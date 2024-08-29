9387b6d63b81e1f33173cf6d68aec16a47741803
mahdi <mahdi.mahdavi.maz@gmail.com>
2024-08-29 13:35:57 +0330

    feat(*): generated link is changed in ci/cd

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -203,7 +203,7 @@ func renderTableResult(vulerabilities map[string][]*types.Vulnerability, pipline
 		}
 	}
 	t.AppendFooter(table.Row{"", "", "Total", count - 1})
-	t.SetCaption(fmt.Sprintf(`Check this out if you need more details: http://37.32.7.91:3030/vulnerabilities/detail/%s`, piplineId.String()))
+	t.SetCaption(fmt.Sprintf(`Check this out if you need more details: http://37.32.7.91:5173/api/vulnerabilities/detail/%s`, piplineId.String()))
 	t.Render()
 
 	return buffer.String()
